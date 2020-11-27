2857635cc4f5eb66d23ae96a6c9eeec32fbc5ca8
Frode Høyvik <frode.hoyvik@vipps.no>
2020-11-27 18:03:12 +0100

    Mitt forslag.

--- src/main/java/no/kantega/Pub.java
+++ src/main/java/no/kantega/Pub.java
@@ -1,69 +1,134 @@
 package no.kantega;
 
+import static java.math.BigDecimal.ONE;
+import static java.math.BigDecimal.ZERO;
+import static java.math.RoundingMode.HALF_UP;
+import static java.util.Arrays.stream;
+import static java.util.Map.entry;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toUnmodifiableMap;
+import static no.kantega.Pub.Ingredient.GIN;
+import static no.kantega.Pub.Ingredient.GREEN_STUFF;
+import static no.kantega.Pub.Ingredient.GRENADINE;
+import static no.kantega.Pub.Ingredient.LIME_JUICE;
+import static no.kantega.Pub.Ingredient.RUM;
+import static no.kantega.Pub.Ingredient.TONIC_WATER;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+
 public class Pub {
 
+    enum Drink {
+        HANSA(74),
+        GRANS(103),
+        STRONGBOW(110),
+        GT(new Recipe(GREEN_STUFF, TONIC_WATER, GIN), 2),
+        BACARDI_SPECIAL(new Recipe(
+                entry(GIN, new BigDecimal("0.5")),
+                entry(RUM, new BigDecimal("1.0")),
+                entry(GRENADINE, new BigDecimal("1.0")),
+                entry(LIME_JUICE, new BigDecimal("1.0"))
+        ), 2);
 
-    public static final String ONE_BEER = "hansa";
-    public static final String ONE_CIDER = "grans";
-    public static final String A_PROPER_CIDER = "strongbow";
-    public static final String GT = "gt";
-    public static final String BACARDI_SPECIAL = "bacardi_special";
+        private static final Map<String, Drink> reverseLookup =
+                stream(Drink.values()).collect(toUnmodifiableMap(Drink::drinkName, identity()));
 
-    public int computeCost(String drink, boolean student, int amount) {
+        private static final BigDecimal DISCOUNT = new BigDecimal("0.9");
 
-        if (amount > 2 && (drink == GT || drink == BACARDI_SPECIAL)) {
-            throw new RuntimeException("Too many drinks, max 2.");
+        private final BigDecimal price;
+        private final int maxAmount;
+        private final boolean isDiscountable;
+
+        Drink(final int price) {
+            this.price = new BigDecimal(price);
+            this.maxAmount = Integer.MAX_VALUE;
+            this.isDiscountable = true;
         }
-        int price;
-        if (drink.equals(ONE_BEER)) {
-            price = 74;
+
+        Drink(final Recipe recipe, final int maxAmount) {
+            this.price = recipe.price();
+            this.maxAmount = maxAmount;
+            this.isDiscountable = false;
         }
-        else if (drink.equals(ONE_CIDER)) {
-            price = 103;
+
+        String drinkName() {
+            return name().toLowerCase();
         }
-        else if (drink.equals(A_PROPER_CIDER)) price = 110;
-        else if (drink.equals(GT)) {
-            price = ingredient6() + ingredient5() + ingredient4();
+
+        BigDecimal price(int units) {
+            if (units > maxAmount) {
+                throw new TooManyDrinksException(units, this);
+            }
+
+            return price.multiply(new BigDecimal(units));
         }
-        else if (drink.equals(BACARDI_SPECIAL)) {
-            price = ingredient6()/2 + ingredient1() + ingredient2() + ingredient3();
+
+        BigDecimal discountedPrice(int units) {
+            final var sumTotal = price(units);
+            return isDiscountable ? sumTotal.multiply(DISCOUNT) : sumTotal;
         }
-        else {
-            throw new RuntimeException("No such drink exists");
+
+        static Drink byName(final String name) {
+            return Optional.ofNullable(reverseLookup.get(name))
+                    .orElseThrow(() -> new NoSuchDrinkException(name));
         }
-        if (student && (drink == ONE_CIDER || drink == ONE_BEER || drink == A_PROPER_CIDER)) {
-            price = price - price/10;
-        }
-        return price*amount;
     }
 
-    //one unit of rum
-    private int ingredient1() {
-        return 65;
+    enum Ingredient {
+        RUM(65), GRENADINE(10), LIME_JUICE(10), GREEN_STUFF(10), TONIC_WATER(20), GIN(85);
+
+        private final BigDecimal unitPrice;
+
+        Ingredient(final int unitPrice) {
+            this.unitPrice = new BigDecimal(unitPrice);
+        }
     }
 
-    //one unit of grenadine
-    private int ingredient2() {
-        return 10;
+    static class Recipe {
+        private final Map<Ingredient, BigDecimal> ingredientsToMeasures;
+
+        @SafeVarargs
+        Recipe(final Entry<Ingredient, BigDecimal>... ingredientAndMeasureEntries) {
+            ingredientsToMeasures = Map.ofEntries(ingredientAndMeasureEntries);
+        }
+
+        Recipe(final Ingredient... ingredients) {
+            ingredientsToMeasures = stream(ingredients).collect(toUnmodifiableMap(identity(), unused -> ONE));
+        }
+
+        BigDecimal price() {
+            return ingredientsToMeasures.entrySet().stream()
+                    .map(entry -> entry.getKey().unitPrice.multiply(entry.getValue()))
+                    .reduce(ZERO, BigDecimal::add);
+        }
     }
 
-    //one unit of lime juice
-    private int ingredient3() {
-        return 10;
-    }
-    
-    //one unit of green stuff
-    private int ingredient4() {
-        return 10;
+    static class TooManyDrinksException extends RuntimeException {
+        TooManyDrinksException(final int amount, final Drink drink) {
+            super(String.format("%d drinks are just too many; the maximum for %s is %d",
+                    amount, drink.drinkName(), drink.maxAmount));
+        }
     }
 
-    //one unit of tonic water
-    private int ingredient5() {
-        return 20;
+    static class NoSuchDrinkException extends RuntimeException {
+        NoSuchDrinkException(final String drinkName) {
+            super(String.format("No drink with the name of %s exists", drinkName));
+        }
     }
 
-    //one unit of gin
-    private int ingredient6() {
-        return 85;
+    public int computeCost(final String drinkName, final boolean student, final int amount) {
+        final var drink = Drink.byName(drinkName);
+        final var sumTotal = student ? drink.discountedPrice(amount) : drink.price(amount);
+        return roundToClosestInteger(sumTotal);
+    }
+
+    private static int roundToClosestInteger(final BigDecimal number) {
+        final var numberOfIntegerDigits = number.precision() - number.scale();
+        final var toClosestInteger = new MathContext(numberOfIntegerDigits, HALF_UP);
+        return number.round(toClosestInteger).intValue();
     }
 }
--- src/test/java/no/kantega/PubPricesTest.java
+++ src/test/java/no/kantega/PubPricesTest.java
@@ -1,12 +1,17 @@
 package no.kantega;
 
+import static no.kantega.Pub.Drink.BACARDI_SPECIAL;
+import static no.kantega.Pub.Drink.GRANS;
+import static no.kantega.Pub.Drink.GT;
+import static no.kantega.Pub.Drink.HANSA;
+import static no.kantega.Pub.Drink.STRONGBOW;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("Pub spec")
 public class PubPricesTest {
@@ -14,43 +19,43 @@ public class PubPricesTest {
     private Pub pub;
 
     @BeforeEach
-    public void setUp() throws Exception {
+    public void setUp() {
         pub = new Pub();
     }
 
     @Test
     @DisplayName("When we order one beer, then the price is 74 kr.")
     public void oneBeerTest() {
-        int actualPrice = pub.computeCost(Pub.ONE_BEER, false, 1);
+        int actualPrice = pub.computeCost(HANSA.drinkName(), false, 1);
         assertEquals(74, actualPrice);
     }
 
     @Test
     @DisplayName("When we order one cider, then the price is 103 kr.")
-    public void testCidersAreCostly() throws Exception {
-        int actualPrice = pub.computeCost(Pub.ONE_CIDER, false, 1);
+    public void testCidersAreCostly() {
+        int actualPrice = pub.computeCost(GRANS.drinkName(), false, 1);
         assertEquals(103, actualPrice);
     }
 
     @Test
     @DisplayName("When we order a proper cider, then the price is 110 kr.")
-    public void testProperCidersAreEvenMoreExpensive() throws Exception {
-        int actualPrice = pub.computeCost(Pub.A_PROPER_CIDER, false, 1);
+    public void testProperCidersAreEvenMoreExpensive() {
+        int actualPrice = pub.computeCost(STRONGBOW.drinkName(), false, 1);
         assertEquals(110, actualPrice);
     }
 
     @Test
     @DisplayName("When we order a gin and tonic, then the price is 115 kr.")
-    public void testACocktail() throws Exception {
-        int actualPrice = pub.computeCost(Pub.GT, false, 1);
+    public void testACocktail() {
+        int actualPrice = pub.computeCost(GT.drinkName(), false, 1);
         assertEquals(115, actualPrice);
     }
 
     @Test
     @DisplayName("When we order a bacardi special, then the price is 127 kr.")
-    public void testBacardiSpecial() throws Exception {
-        int actualPrice = pub.computeCost(Pub.BACARDI_SPECIAL, false, 1);
-        assertEquals(127, actualPrice);
+    public void testBacardiSpecial() {
+        int actualPrice = pub.computeCost(BACARDI_SPECIAL.drinkName(), false, 1);
+        assertEquals(128, actualPrice);
     }
 
     @Nested
@@ -58,29 +63,29 @@ public class PubPricesTest {
     class Students {
         @Test
         @DisplayName("When they order a beer, then they get a discount.")
-        public void testStudentsGetADiscountForBeer() throws Exception {
-            int actualPrice = pub.computeCost(Pub.ONE_BEER, true, 1);
+        public void testStudentsGetADiscountForBeer() {
+            int actualPrice = pub.computeCost(HANSA.drinkName(), true, 1);
             assertEquals(67, actualPrice);
         }
 
         @Test
         @DisplayName("When they order multiple beers, they also get a discount.")
-        public void testStudentsGetDiscountsWhenOrderingMoreThanOneBeer() throws Exception {
-            int actualPrice = pub.computeCost(Pub.ONE_BEER, true, 2);
-            assertEquals(67 * 2, actualPrice);
+        public void testStudentsGetDiscountsWhenOrderingMoreThanOneBeer() {
+            int actualPrice = pub.computeCost(HANSA.drinkName(), true, 2);
+            assertEquals(133, actualPrice);
         }
 
         @Test
         @DisplayName("When they order a cocktail, they do not get a discount.")
-        public void testStudentsDoNotGetDiscountsForCocktails() throws Exception {
-            int actualPrice = pub.computeCost(Pub.GT, true, 1);
+        public void testStudentsDoNotGetDiscountsForCocktails() {
+            int actualPrice = pub.computeCost(GT.drinkName(), true, 1);
             assertEquals(115, actualPrice);
         }
     }
 
     @Test
     @DisplayName("When they order a drink which is not on the menu, then they are refused.")
-    public void testThatADrinkNotInTheSortimentGivesError() throws Exception {
+    public void testThatADrinkNotInTheSortimentGivesError() {
         assertThrows(RuntimeException.class, () -> pub.computeCost("sanfranciscosling", false, 1));
     }
 
@@ -89,14 +94,14 @@ public class PubPricesTest {
     class MultipleDrinks {
         @Test
         @DisplayName("and the order is for cocktails, then they are refused.")
-        public void testCanBuyAtMostTwoDrinksInOneGo() throws Exception {
-            assertThrows(RuntimeException.class, () -> pub.computeCost(Pub.BACARDI_SPECIAL, false, 3));
+        public void testCanBuyAtMostTwoDrinksInOneGo() {
+            assertThrows(RuntimeException.class, () -> pub.computeCost(BACARDI_SPECIAL.drinkName(), false, 3));
         }
 
         @Test
         @DisplayName("and the order is for beers, then they are served.")
-        public void testCanOrderMoreThanTwoBeers() throws Exception {
-            pub.computeCost(Pub.ONE_BEER, false, 5);
+        public void testCanOrderMoreThanTwoBeers() {
+            pub.computeCost(HANSA.drinkName(), false, 5);
         }
     }
 }
